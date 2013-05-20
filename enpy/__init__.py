fa9caa90a315a3635b4a0aba427ef259fae4869f
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-05-19 23:22:36 -0700

    adding init to pkg

---
+++ enpy/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+"""
+    enpy
+"""
+
+__version__ = "0.0.1"
+__author__ = [
+    "Mek <michael.karpeles@gmail.com>"
+]
+__license__ = "public domain"
+__contributors__ = "see AUTHORS"
